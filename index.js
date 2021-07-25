4bd5d869d6ef66ee0a26acec6bff3454d9a09863
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-07-25 22:04:35 +0430

    add token route

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
+let currentUserGitlabAccessToken;
+let currentUserGitlabId;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
 app.post('/token', (req, res) => {
-	console.log(req.body);
+	currentUserGitlabId = req.body.userId;
+	currentUserGitlabAccessToken = req.body.gitlabAccessToken;
+	res.status(200).send('Got the token');
 });
 
 app.listen(process.env.PORT || 3000, () => {
