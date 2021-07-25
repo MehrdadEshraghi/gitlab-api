e5a8041433e3e7830d5e9f55dda5b7f752415dca
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-07-26 00:37:39 +0430

    add gitlab repository route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
@@ -9,6 +10,27 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
+});
+
+app.get('/projects/:id/repository_tree', async (req, res) => {
+	const userId = req.header('userId');
+	if (userId && userId === currentUserGitlabId) {
+		try {
+			const response = await axios.get(`https://gitlab.com/api/v4/projects/${req.params.id}/repository/tree`, {
+				headers: {
+					Authorization: `Bearer ${currentUserGitlabAccessToken}`
+				}
+			});
+			res.status(200).send(response.data);
+		} catch (err) {
+			console.log(err);
+			const message = { message: `${err.message} ${err.response.statusText}` };
+			res.status(err.response.status).send(message);
+		}
+	} else {
+		const message = { message: '400 Bad Request' };
+		res.status(400).send(message);
+	}
 });
 
 app.post('/token', (req, res) => {
