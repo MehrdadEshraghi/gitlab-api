69968e39e4e5380a712d9897cb34db0b643fe2a2
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2021-07-25 19:51:35 +0430

    install nodemon

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.post('/token', (req, res) => {
+	console.log(req.body);
+});
+
 app.listen(process.env.PORT || 3000, () => {
 	console.log('Server Listening...');
 });
